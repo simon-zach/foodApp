085fc46ab1d36c7f8bb26e860d2869d36888d341
simon-zach <zacharczukszymon@gmail.com>
2021-12-22 19:30:47 +0100

    yelp tested

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'react-bootstrap';
+
+const apiKey='O9bsGMTU-bFVTohrdL3E7bWnVANCxhmaV0cb_mMDbmMpPHGKIe9MD5hsO0-aTKTjQiNbiz1afN6Onpa39CLYk4Y0zOxhlXC7mwKGw1-a6GY3HWdFL3s3Jt6Rh2TDYXYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      }
+    }).then(response => {
+      if (response){
+        return response.json();
+      }
+      
+    })
+  }
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>hello</h1>
       </header>
+     <Button onClick={()=>Yelp.search('food','pleszew').then(response=>console.log(response))} variant="primary">Button test</Button>
     </div>
   );
 }
